9744cb04e878c4d6d3b94f803d108780618eb604
Michael Simmons <msimmons86@gmail.com>
2022-04-01 10:03:04 -0700

    commit tree 789726d55b2eb611a848ebbd10af7d86d59d1e0d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let newArr = [];
+
+function writeCards(names, event) {
+ for (let i = 0; i < names.length; i++) {
+   newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+ return newArr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num)
+    num--
+  }
+}
+
